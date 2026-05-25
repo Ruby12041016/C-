9106b363ed37dce75cececde8177c016bbb06f41
Richard-afka <2671530264@qq.com>
2026-05-25 22:04:30 +0800

    4

---
+++ leecode/矩阵置零.cc
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<bool> row(m, 0);
+        vector<bool> col(n, 0);
+
+        // 第一遍：记录哪些行和列需要置零
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // 第二遍：根据记录置零
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
